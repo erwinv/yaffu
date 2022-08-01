f470be9ce5273229dee246f828de0b6366b4bd54
Erwin Villejo <erwinvillejo@gmail.com>
2022-08-01 18:53:46 +0700

    add concat demux to demo

--- example/index.mjs
+++ example/index.mjs
@@ -1,7 +1,7 @@
 import { range, take } from 'lodash-es'
 import { constants } from 'fs'
 import { access } from 'fs/promises'
-import { genericCombine, ffmux } from 'yaffu'
+import { genericCombine, ffmux, ffconcatDemux } from 'yaffu'
 import { downloadFile } from './util.mjs'
 
 const file = 'BigBuckBunny-720p.mp4'
@@ -20,11 +20,17 @@ async function maybeDownloadInputFile() {
 async function main() {
   await maybeDownloadInputFile()
 
-  const inputs = range(16).map((i) => [file, [`-ss ${i * 10}`, '-t 10']])
+  const inputOpts = range(16).map((i) => [`-ss ${i * 10}`, '-t 10'])
+  const outputs = range(1, 17).map(i => `combined_${i}.mp4`)
 
   for (const i of range(1, 17)) {
-    await ffmux(genericCombine(take(inputs, i), `combined_${i}.mp4`))
+    const inputs = take(inputOpts, i)
+      .map(opts => [file, opts])
+      .reverse()
+    await ffmux(genericCombine(inputs, outputs[i]))
   }
+
+  await ffconcatDemux(outputs, 'combined_concatenated.mp4')
 }
 
 main()
